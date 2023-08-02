17c0be6e437cf0f2407b5c135ae8298a0c76ec7c
Benjamin Walkenhorst <krylon@posteo.de>
2023-08-02 20:00:10 +0200

    Add test for QueueQueryStatus

--- monitor/01_monitor_test.go
+++ monitor/01_monitor_test.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 10. 07. 2023 by Benjamin Walkenhorst
 // (c) 2023 Benjamin Walkenhorst
-// Time-stamp: <2023-07-17 21:29:51 krylon>
+// Time-stamp: <2023-08-02 19:57:24 krylon>
 
 package monitor
 
@@ -20,6 +20,12 @@ import (
 )
 
 const netname = "unixpacket"
+
+var directories = []string{
+	"/etc",
+	"/usr/lib",
+	"/usr/include",
+}
 
 var mon *Monitor
 
@@ -46,26 +52,16 @@ func TestMonCreate(t *testing.T) {
 
 func TestMonSubmit(t *testing.T) {
 	var (
-		err         error
-		raddr       net.UnixAddr
-		conn        *net.UnixConn
-		rcvbuf      = make([]byte, 65536)
-		directories = []string{
-			"/etc",
-			"/usr/lib",
-			"/usr/include",
-		}
+		err    error
+		raddr  net.UnixAddr
+		conn   *net.UnixConn
+		rcvbuf = make([]byte, 65536)
 	)
 
 	raddr = net.UnixAddr{
 		Net:  netname,
 		Name: socketPath,
 	}
-
-	// laddr = net.UnixAddr{
-	// 	Net:  netname,
-	// 	Name: "client",
-	// }
 
 	if conn, err = net.DialUnix(netname, nil, &raddr); err != nil {
 		t.Fatalf("Error connecting to Monitor %s: %s",
@@ -74,8 +70,6 @@ func TestMonSubmit(t *testing.T) {
 	}
 
 	defer conn.Close() // nolint: errcheck
-
-	// t.Logf("LOCAL ADDRESS = %s", laddr)
 
 	for _, d := range directories {
 		var (
@@ -118,3 +112,50 @@ func TestMonSubmit(t *testing.T) {
 
 	time.Sleep(time.Second * 10)
 } // func TestMonSubmit(t *testing.T)
+
+func TestMonQuery(t *testing.T) {
+	var (
+		err    error
+		raddr  net.UnixAddr
+		conn   *net.UnixConn
+		rcvbuf = make([]byte, 65536)
+		sndbuf []byte
+		cnt    int
+		msg    Message
+		res    Response
+	)
+
+	raddr = net.UnixAddr{
+		Net:  netname,
+		Name: socketPath,
+	}
+
+	if conn, err = net.DialUnix(netname, nil, &raddr); err != nil {
+		t.Fatalf("Error connecting to Monitor %s: %s",
+			socketPath,
+			err.Error())
+	}
+
+	defer conn.Close() // nolint: errcheck
+
+	msg = MakeMsg(request.QueueQueryStatus.String(), nil)
+
+	if sndbuf, err = json.Marshal(&msg); err != nil {
+		t.Fatalf("Cannot serialize Job: %s",
+			err.Error())
+	} else if _, err = conn.Write(sndbuf); err != nil {
+		t.Errorf("Cannot send JSON payload to Monitor: %s",
+			err.Error())
+	} else if cnt, err = conn.Read(rcvbuf); err != nil {
+		t.Errorf("Cannot receive reply from Monitor: %s",
+			err.Error())
+	} else if err = json.Unmarshal(rcvbuf[:cnt], &res); err != nil {
+		t.Errorf("Failed to decode Response: %s\n\n%s",
+			err.Error(),
+			string(rcvbuf[:cnt]))
+	} else if len(res.Jobs) != len(directories) {
+		t.Errorf("Unexpected number of jobs: %d Expected %d",
+			len(res.Jobs),
+			len(directories))
+	}
+} // func TestMonQuery(t *testing.T)
