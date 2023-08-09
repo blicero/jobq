d6c5cc04d95b1458ff6e7ed89248270d5c5c1d51
Benjamin Walkenhorst <krylon@posteo.de>
2023-08-09 21:08:34 +0200

    main.go: Hand over to the cli.CLI

--- main.go
+++ main.go
@@ -2,13 +2,15 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 18. 06. 2023 by Benjamin Walkenhorst
 // (c) 2023 Benjamin Walkenhorst
-// Time-stamp: <2023-06-19 20:23:09 krylon>
+// Time-stamp: <2023-08-09 20:53:55 krylon>
 
 package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/blicero/jobq/cli"
 	"github.com/blicero/jobq/common"
 )
 
@@ -18,5 +20,18 @@ func main() {
 		common.Version,
 		common.BuildStamp.Format(common.TimestampFormat))
 
-	fmt.Println("Nothing to see here (so far), move along...")
+	var (
+		err   error
+		shell *cli.CLI
+	)
+
+	if shell, err = cli.Create(); err != nil {
+		fmt.Fprintf(
+			os.Stderr,
+			"Failed to create CLI handler: %s\n",
+			err.Error())
+		os.Exit(1)
+	}
+
+	shell.Execute()
 }
